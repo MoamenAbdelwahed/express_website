dd40ed6cf4169e5d657a0e3b3f8e71facd94790d
MoamenAbdelwahed <moomen.abdelwahed@gmail.com>
2016-05-10 15:34:30 +0200

    using nodemailer for contacts form

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
+});
+
+router.post('/send', function(req, res, next) {
+  var transporter = nodemailer.createTransport({
+  	service: 'Gmail',
+  	auth: {
+  		user: 'moamen4development@gmail.com',
+  		pass: 'qaz/123456789',
+  	}
+  });
+  var mailOptions = {
+  	from: 'My express website',
+  	to: 'moomen.abdelwahed@gmail.com',
+  	subject: 'Website Submission',
+  	test: 'You have a new submission with the following details..Name: '+req.body.name+' Email: '+req.body.email+' Message: '+req.body.message+'',
+  	html: '<p>You have a new submission with the following details..</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>',
+  };
+
+  transporter.sendMail(mailOptions,function(error, info){
+  	if(error){
+  		console.log(error);
+  		res.redirect('/');
+  	}else{
+  		console.log("Message sent: "+info.response);
+  		res.redirect('/');
+  	}
+  });
 });
 
 module.exports = router;
